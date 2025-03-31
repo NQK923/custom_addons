704d126573b36a985db490499539d665bc82dc67
NQK923 <nqk090203@gmai.colm>
2025-03-31 09:56:26 +0700

    xml to website template(prescription).

--- prescription_management/controllers/controllers.py
+++ prescription_management/controllers/controllers.py
@@ -1,22 +1,41 @@
-# -*- coding: utf-8 -*-
-# from odoo import http
+from odoo import http
+from odoo.http import request
 
 
-# class Medical-manager(http.Controller):
-#     @http.route('/medical-manager/medical-manager', auth='public')
-#     def index(self, **kw):
-#         return "Hello, world"
+class PrescriptionManagementController(http.Controller):
+    @http.route('/pharmacy/prescriptions', type='http', auth='public', website=True, methods=['GET', 'POST'])
+    def prescription_list(self, **kwargs):
+        search_value = kwargs.get('search_value', '')
+        prescriptions = False
+        patient = False
 
-#     @http.route('/medical-manager/medical-manager/objects', auth='public')
-#     def list(self, **kw):
-#         return http.request.render('medical-manager.listing', {
-#             'root': '/medical-manager/medical-manager',
-#             'objects': http.request.env['medical-manager.medical-manager'].search([]),
-#         })
+        if search_value:
+            # Search for patient by code or name
+            patient = request.env['clinic.patient'].sudo().search([
+                '|', ('code', 'ilike', search_value), ('name', 'ilike', search_value)
+            ], limit=1)
 
-#     @http.route('/medical-manager/medical-manager/objects/<model("medical-manager.medical-manager"):obj>', auth='public')
-#     def object(self, obj, **kw):
-#         return http.request.render('medical-manager.object', {
-#             'object': obj
-#         })
+            if patient:
+                # Get prescriptions for this patient
+                prescriptions = request.env['prescription.order'].sudo().search([
+                    ('patient_id', '=', patient.id)
+                ], order='date desc')
 
+        values = {
+            'patient': patient,
+            'prescriptions': prescriptions,
+            'search_value': search_value,
+        }
+        return request.render('prescription_management.prescription_list_template', values)
+
+    @http.route('/pharmacy/prescription/<model("prescription.order"):prescription_id>', type='http', auth='public',
+                website=True)
+    def prescription_detail(self, prescription_id, **kwargs):
+        if not prescription_id:
+            return request.redirect('/pharmacy/prescriptions')
+
+        values = {
+            'prescription': prescription_id,
+            'prescription_lines': prescription_id.prescription_line_ids,
+        }
+        return request.render('prescription_management.prescription_detail_template', values)
