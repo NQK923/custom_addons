769dc72304aae7fcf0152a2576f04b765f58905d
NQK923 <nqk090203@gmail.com>
2025-03-31 09:56:26 +0700

    xml to website template(prescription).

--- prescription_management/__manifest__.py
+++ prescription_management/__manifest__.py
@@ -4,13 +4,14 @@
     'category': 'Healthcare',
     'summary': 'Manage pharmacy inventory and stock operations',
     'description': 'A module to manage pharmacy inventory, stock in/out, and ensure sufficient medicine supply for patients.',
-    'depends': ['base'],
+    'depends': ['base', 'website'],
     'data': [
         'security/ir.model.access.csv',
-        'views/views.xml',  # Chỉ load file này
+        'views/views.xml',
+        'views/prescription_templates.xml',
+        'views/website_menu.xml',
     ],
     'installable': True,
     'application': True,
     'license': 'LGPL-3',
-
 }
