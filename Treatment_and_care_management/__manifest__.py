7ff046dec01549c371b9190141170cb7c349659f
NQK923 <nqk090203@gmai.colm>
2025-04-02 19:08:08 +0700

    xml to website template(treatment).

--- Treatment_and_care_management/__manifest__.py
+++ Treatment_and_care_management/__manifest__.py
@@ -4,14 +4,19 @@
     'category': 'Healthcare',
     'summary': 'Manage pharmacy inventory and stock operations',
     'description': 'A module to manage pharmacy inventory, stock in/out, and ensure sufficient medicine supply for patients.',
-    'depends': ['base', "patient_management"],
+    'depends': ['base', "patient_management", "website"],  # Added website dependency
     'data': [
         'security/ir.model.access.csv',
-        'views/views.xml',  # Chỉ load file này
+        'views/views.xml',  # Keep for backward compatibility
         "views/clinic_patient_views.xml",
+        'views/website_templates/treatment_plans_template.xml',
+        'views/website_templates/patient_care_template.xml',
+        'views/website_templates/treatment_plan_forms.xml',
+        'views/website_templates/treatment_process_forms.xml',
+        'views/website_templates/patient_care_forms.xml',
+        'views/website_menu.xml',
     ],
     'installable': True,
     'application': True,
     'license': 'LGPL-3',
-
 }
