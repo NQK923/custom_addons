70c7be5dc6f5f7380ee3d23d8a2e784eea9ef244
NQK923 <nqk090203@gmai.colm>
2025-04-02 19:19:00 +0700

    xml to website template(treatment).

--- Treatment_and_care_management/__manifest__.py
+++ Treatment_and_care_management/__manifest__.py
@@ -7,13 +7,11 @@
     'depends': ['base', "patient_management", "website"],  # Added website dependency
     'data': [
         'security/ir.model.access.csv',
-        'views/views.xml',  # Keep for backward compatibility
-        "views/clinic_patient_views.xml",
-        'views/website_templates/treatment_plans_template.xml',
-        'views/website_templates/patient_care_template.xml',
-        'views/website_templates/treatment_plan_forms.xml',
-        'views/website_templates/treatment_process_forms.xml',
-        'views/website_templates/patient_care_forms.xml',
+        'views/treatment_plans_template.xml',
+        'views/patient_care_template.xml',
+        'views/treatment_plan_forms.xml',
+        'views/treatment_process_forms.xml',
+        'views/patient_care_forms.xml',
         'views/website_menu.xml',
     ],
     'installable': True,
--- app_dashboard/models/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import my_custom_model
-
-
