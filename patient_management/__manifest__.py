0f7479c99fc0a24a37ab9f006232090450806efb
NQK923 <nqk090203@gmail.com>
2025-04-03 14:24:31 +0700

    xml to website template(heath_care).

--- patient_management/__manifest__.py
+++ patient_management/__manifest__.py
@@ -10,7 +10,7 @@
     'category': 'Healthcare',
     'version': '0.1',
     'license': 'LGPL-3',
-    'depends': ['base', 'website', 'insurance_management'],
+    'depends': ['base', 'website'],
     'data': [
         'security/ir.model.access.csv',
         'views/clinic_patient_views.xml',
