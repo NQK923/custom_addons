94db306da16172866782bd4c971d68934d0afe09
NQK923 <nqk090203@gmail.com>
2025-04-20 07:36:25 +0700

    refactor code

--- healthcare_management/models/__init__.py
+++ healthcare_management/models/__init__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 
 from . import appointment_reminder
+from . import patient_complaint
+from . import patient_feedback
 from . import complaint_dashboard
 from . import complaint_statistics
 from . import feedback_dashboard
 from . import feedback_statistics
-from . import patient_complaint
-from . import patient_feedback
